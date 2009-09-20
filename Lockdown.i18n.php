722b9fcab8d331c71d789b9a0358d1a1a6485572
Siebrand Mazeland <siebrand@users.mediawiki.org>
2009-09-20 23:09:21 +0000

    Localisation updates for extension messages from translatewiki.net (2009-09-20 22:58 UTC)

--- Lockdown.i18n.php
+++ Lockdown.i18n.php
@@ -15,10 +15,94 @@ $messages['en'] = array(
 	'lockdown-desc' => 'Per namespace group permissions',
 );
 
+/** Message documentation (Message documentation)
+ * @author Fryed-peach
+ */
+$messages['qqq'] = array(
+	'lockdown-desc' => '{{desc}}',
+);
+
+/** Belarusian (Taraškievica orthography) (Беларуская (тарашкевіца))
+ * @author Jim-by
+ */
+$messages['be-tarask'] = array(
+	'lockdown-desc' => 'Групавыя дазволы па прасторам назваў',
+);
+
+/** Breton (Brezhoneg)
+ * @author Fulup
+ */
+$messages['br'] = array(
+	'lockdown-desc' => 'Aotreoù moned dre esaouenn anv',
+);
+
+/** French (Français)
+ * @author IAlex
+ */
+$messages['fr'] = array(
+	'lockdown-desc' => "Permissions d'accès par espace de noms",
+);
+
+/** Galician (Galego)
+ * @author Toliño
+ */
+$messages['gl'] = array(
+	'lockdown-desc' => 'Permisos de acceso por espazo de nomes',
+);
+
+/** Upper Sorbian (Hornjoserbsce)
+ * @author Michawiki
+ */
+$messages['hsb'] = array(
+	'lockdown-desc' => 'Skupinske prawa za mjenowy rum',
+);
+
+/** Hungarian (Magyar)
+ * @author Glanthor Reviol
+ */
+$messages['hu'] = array(
+	'lockdown-desc' => 'Névterenkénti csoportjogosultságok',
+);
+
+/** Japanese (日本語)
+ * @author Fryed-peach
+ */
+$messages['ja'] = array(
+	'lockdown-desc' => '名前空間ごとのグループ許可',
+);
+
+/** Ripoarisch (Ripoarisch)
+ * @author Purodha
+ */
+$messages['ksh'] = array(
+	'lockdown-desc' => 'Einzel Jroppe-Rääschte pro Appachtemang',
+);
+
+/** Luxembourgish (Lëtzebuergesch)
+ * @author Robby
+ */
+$messages['lb'] = array(
+	'lockdown-desc' => 'Gruppen-Autorisatioune pro Nummraum',
+);
+
 /** Dutch (Nederlands)
  * @author Siebrand
  */
 $messages['nl'] = array(
 	'lockdown-desc' => 'Groepsrechten per naamruimte',
+);
+
+/** Piedmontese (Piemontèis)
+ * @author Dragonòt
+ */
+$messages['pms'] = array(
+	'lockdown-desc' => 'Përmess për partìe dë spassi nominaj',
+);
+
+/** Tarandíne (Tarandíne)
+ * @author Joetaras
+ */
+$messages['roa-tara'] = array(
+	'lockdown-desc' => 'Permesse pe le gruppe de le namespace',
 );
 
