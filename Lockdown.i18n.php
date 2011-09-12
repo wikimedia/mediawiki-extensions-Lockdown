72157f582268a60c85dfe581dc24a650b5b9ed1f
Raimond Spekking <raymond@users.mediawiki.org>
2011-09-12 21:47:32 +0000

    Localisation updates for core and extension messages from translatewiki.net

--- Lockdown.i18n.php
+++ Lockdown.i18n.php
@@ -84,7 +84,7 @@ $messages['cs'] = array(
  * @author Kghbln
  */
 $messages['de'] = array(
-	'lockdown-desc' => 'Ermöglicht die namensraumspezifische Vergaben von Benutzergruppenberechtigungen',
+	'lockdown-desc' => 'Ermöglicht die benutzergruppenspezifische Vergaben von Berechtigungen für Namensräume, Spezialseiten und Aktionen',
 );
 
 /** Lower Sorbian (Dolnoserbski)
