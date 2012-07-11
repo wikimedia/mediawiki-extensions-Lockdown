3fb1cf9bbbbf6af25ea5c97dd152f9e2b6cc9f70
Siebrand Mazeland <siebrand@users.mediawiki.org>
2012-07-11 07:30:24 +0000

    Localisation updates from http://translatewiki.net.

--- Lockdown.i18n.php
+++ Lockdown.i18n.php
@@ -228,7 +228,7 @@ $messages['ms'] = array(
 	'lockdown-desc' => 'Keizinan kumpulan seruang nama',
 );
 
-/** Norwegian (bokmål)‬ (‪Norsk (bokmål)‬)
+/** Norwegian Bokmål (‪norsk (bokmål)‬)
  * @author Nghtwlkr
  */
 $messages['nb'] = array(
