d9d07ae7bbf37c33e48220399547a29ea9e41c76
Karsten Hoffmeyer <karsten@hoffmeyer.info>
2020-03-14 12:19:03 +0100

    Remove call to non-existing alias file
    Lockdown does not provide a special page thus no
    alias file for it is needed.
    
    Bug: T234010
    Change-Id: Ia0d1af7e67c16dc7e1bd0baea5d10cf2361d5e7d

--- Lockdown.php
+++ Lockdown.php
@@ -3,7 +3,6 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	wfLoadExtension( 'Lockdown' );
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
 	$wgMessagesDirs['Lockdown'] = __DIR__ . '/i18n';
-	$wgExtensionMessagesFiles['LockdownAlias'] = __DIR__ . '/Lockdown.alias.php';
 	wfWarn(
 		'Deprecated PHP entry point used for the Lockdown extension. ' .
 		'Please use wfLoadExtension instead, ' .
